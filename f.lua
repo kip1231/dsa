001c2bb9c44500ef624667973d1fa7e5ade62bb3
kip1231 <81984493+kip1231@users.noreply.github.com>
2024-06-21 22:13:18 +0500

    Update f.lua

--- f.lua
+++ f.lua
@@ -1,3 +1,4 @@
+print("пенис")
 local lib = {RainbowColorValue = 0, HueSelectionPosition = 0}
 local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
